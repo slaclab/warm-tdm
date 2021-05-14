e2e544e33f71c71d71efd3b34f3baf42c775eb95
Benjamin Reese <bareese@slac.stanford.edu>
2021-05-14 11:12:46 -0700

    Add Ethernet devices

--- firmware/common/python/warm_tdm/_ComCore.py
+++ firmware/common/python/warm_tdm/_ComCore.py
@@ -2,6 +2,9 @@ import pyrogue as pr
 
 import surf.protocols.pgp
 import surf.protocols.ssi
+import surf.protocols.rssi
+import surf.ethernet.gige
+import surf.ethernet.udp
 
 import warm_tdm
 
@@ -17,6 +20,24 @@ class PgpCore(pr.Device):
 class EthCore(pr.Device):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        
+        self.add(surf.protocols.rssi.RssiCore(
+            name = "SRP RSSI",
+            offset = 0x11000,
+            expand = False))
+
+        self.add(surf.protocols.rssi.RssiCore(
+            name = "Data RSSI",
+            offset = 0x12000,
+            expand = False))
+        
+        self.add(surf.ethernet.udp.UdpEngine(
+            offset = 0x10000,
+            numSrv = 2))
+
+        self.add(surf.ethernet.gige.GigEthGtx7(
+            gtxe2_read_only = True,
+            offset = 0x00000))
 
 
 class ComCore(pr.Device):
