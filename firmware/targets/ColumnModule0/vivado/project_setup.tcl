e30a806d9c3fc5a7bdb77e64de6559162334d909
Benjamin Reese <bareese@slac.stanford.edu>
2021-11-03 09:28:38 -0700

    Turn off Performance Explore

--- firmware/targets/ColumnModule0/vivado/project_setup.tcl
+++ firmware/targets/ColumnModule0/vivado/project_setup.tcl
@@ -9,7 +9,7 @@
 ##############################################################################
 
 set_property STEPS.SYNTH_DESIGN.ARGS.FLATTEN_HIERARCHY none [get_runs synth_1]
-set_property strategy Performance_Explore [get_runs impl_1]
+#set_property strategy Performance_Explore [get_runs impl_1]
 
 #set_property STEPS.SYNTH_DESIGN.ARGS.BUFG 32 [get_runs synth_1]
 
