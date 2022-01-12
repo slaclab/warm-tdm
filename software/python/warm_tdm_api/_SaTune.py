d8811116e38168cd2832d5f49b274c33e51a74cd
Ryan Herbst <rherbst@slac.stanford.edu>
2022-01-11 21:42:00 -0800

    Update docs

--- software/python/warm_tdm_api/_SaTune.py
+++ software/python/warm_tdm_api/_SaTune.py
@@ -8,24 +8,28 @@ class SaTuneProcess(pr.Process):
         self._maxBiasSteps=maxBiasSteps
 
         # Init master class
-        pr.Process.__init__(self, function=self._saTuneWrap, **kwargs)
-
-        self.add(pr.LocalVariable(name='TestString',
-                                  value='',
-                                  mode='RW',
-                                  description=""))
+        pr.Process.__init__(self, function=self._saTuneWrap,
+                            description='Process which performs an SA tuning step.'
+                                        'This tuning process sweeps the SaFb value and determines the SaOffset value required zero out the SaOut '
+                                        'value. This sweep is repeated for a series of SaBias values. Once the sweep is completed the process will '
+                                        'determine the SaBias value which results in the largest peak to peak amplitude in the SaFb vs SaOffset curve. '
+                                        'For the select SaBias curce the process will then select the point on the curve with the highest slope.'
+                                        'The resulting curves are available as a dictionary of numpy arrays.',
+                            **kwargs)
 
         # Low offset for SA FB Tuning
         self.add(pr.LocalVariable(name='SaFbLowOffset',
                                   value=0.0,
                                   mode='RW',
-                                  description="Starting point offset for SA FB Tuning"))
+                                  description='Starting point offset for SA FB Tuning. This value is an offset from the currently configured '
+                                              'SaFb value. (well not in the current code, but it should be). '))
 
         # High offset for SA FB Tuning
         self.add(pr.LocalVariable(name='SaFbHighOffset',
                                   value=1.0,
                                   mode='RW',
-                                  description="Ending point offset for SA FB Tuning"))
+                                  description='Ending point offset for SA FB Tuning. This value is an offset from the currently configured '
+                                              'SaFb value. (well not in the current code, but it should be). '))
 
         # Step size for SA FB Tuning
         self.add(pr.LocalVariable(name='SaFbNumSteps',
@@ -33,7 +37,7 @@ class SaTuneProcess(pr.Process):
                                   maximum=10000,
                                   value=1000,
                                   mode='RW',
-                                  description="Number of steps for SA FB Tuning"))
+                                  description='Number of steps between the SaFbLowOffset and SaFbHighOffset, inclusively.'))
 
         # Wait time between FB set and output sampling
         self.add(pr.LocalVariable(name='SaFbSampleDelay',
@@ -46,13 +50,15 @@ class SaTuneProcess(pr.Process):
         self.add(pr.LocalVariable(name='SaBiasLowOffset',
                                   value=0.0,
                                   mode='RW',
-                                  description="Starting point offset for SA Bias Tuning"))
+                                  description='Starting point offset for SA Bias Tuning. This value is an offset from the currently configured '
+                                              'SaBias value. (well not in the current code, but it should be). '))
 
         # High offset for SA Bias Tuning
         self.add(pr.LocalVariable(name='SaBiasHighOffset',
                                   value=1.0,
                                   mode='RW',
-                                  description="Ending point offset for SA Bias Tuning"))
+                                  description='Ending point offset for SA Bias Tuning. This value is an offset from the currently configured '
+                                              'SaBias value. (well not in the current code, but it should be). '))
 
         # Step size for SA Bias Tuning
         self.add(pr.LocalVariable(name='SaBiasNumSteps',
@@ -60,13 +66,29 @@ class SaTuneProcess(pr.Process):
                                   maximum=self._maxBiasSteps,
                                   value=10,
                                   mode='RW',
-                                  description="Number of steps for SA Bias Tuning"))
+                                  description='Number of steps between the SaBiasLowOffset and SaBiasHighOffset, inclusively.'))
 
         # Set values after finish
         self.add(pr.LocalVariable(name='SetAfterFinish',
                                   value=False,
                                   mode='RW',
-                                  description="Set the values after finish"))
+                                  description="This variale controls if the tuning point found at the end of the process is set back. "
+                                              "Otherwise the previous values of SaFb and SaBias will be restored."))
+
+        # SA Tuning Results
+        self.add(pr.LocalVariable(name='SaTuneOutput',
+                                  hidden=True,
+                                  value={},
+                                  mode='RO',
+                                  description="Results Data From SA Tuning. "
+                                              "This is a list of dictionaries, with one dictionary for each column in the system (ColumBoards * 8). "
+                                              "Each dctionary contains the following fields: . "
+                                              "xValues: x-axis values (SaFb) for each curve. "
+                                              "biasValues: array of SaBias values, one for each SaBias step. "
+                                              "curves: SaOffset vs SaFb curves, one for each SaBias value. "
+                                              "biasOut: Selected SaBias value after fitting. "
+                                              "fbOut: Selected SaFb value after fitting. "
+                                              "offsetOut: Selected SaOffset out value after fitting. "))
 
         # Select Channel
         self.add(pr.LocalVariable(name='PlotColumn',
@@ -75,42 +97,36 @@ class SaTuneProcess(pr.Process):
                                   maximum=len(config.columnMap)-1,
                                   hidden=True,
                                   mode='RW',
-                                  description="Channel Number To Plot"))
+                                  description="Controls which column is selected for the resulting plot and fitted value variables below"))
 
-        # SA Tuning Results
-        self.add(pr.LocalVariable(name='SaTuneOutput',
-                                  hidden=True,
-                                  value={},
-                                  mode='RO',
-                                  description="Results Data From SA Tuning"))
 
         self.add(pr.LinkVariable(name='FittedSaFb',
                                  mode='RO',
                                  hidden=True,
                                  dependencies=[self.PlotColumn,self.SaTuneOutput],
                                  linkedGet=self._saFbGet,
-                                 description=""))
+                                 description="Fitted SaFB value for the column selected via the PlotColumn variable above. "))
 
         self.add(pr.LinkVariable(name='FittedSaBias',
                                  mode='RO',
                                  hidden=True,
                                  dependencies=[self.PlotColumn,self.SaTuneOutput],
                                  linkedGet=self._saBiasGet,
-                                 description=""))
+                                 description="Fitted SaBias value for the column selected via the PlotColumn variable above. "))
 
         self.add(pr.LinkVariable(name='FittedSaOffset',
                                  mode='RO',
                                  hidden=True,
                                  dependencies=[self.PlotColumn,self.SaTuneOutput],
                                  linkedGet=self._saOffsetGet,
-                                 description=""))
+                                 description="Fitted SaOffset value for the column selected via the PlotColumn variable above. "))
 
         self.add(pr.LinkVariable(name='PlotXData',
                                  mode='RO',
                                  hidden=True,
                                  dependencies=[self.PlotColumn,self.SaTuneOutput],
                                  linkedGet=self._plotXGet,
-                                 description=""))
+                                 description="X-Axis data for the column selected via the PlotColumn variable above. "))
 
         for i in range(self._maxBiasSteps):
             self.add(pr.LinkVariable(name=f'PlotYData[{i}]',
@@ -118,12 +134,12 @@ class SaTuneProcess(pr.Process):
                                      hidden=True,
                                      dependencies=[self.PlotColumn,self.SaTuneOutput],
                                      linkedGet=lambda i=i: self._plotYGet(i),
-                                     description=""))
+                                     description="Y-Axis data for each SaBias value for the column selected via the PlotColumn variable above. "))
 
         self.add(pr.LocalCommand(name='SavePlotData',
                                  value='',
                                  function=self._saveData,
-                                 description=""))
+                                 description="Command to save the plot data as a numpy binary file (np.save). The arg is the filename to write the data to. "))
 
         self.ReadDevice.addToGroup('NoDoc')
         self.WriteDevice.addToGroup('NoDoc')
