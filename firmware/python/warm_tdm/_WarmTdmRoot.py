406cc4e1c0617867ead0be125fc868d5c4af300a
Benjamin Reese <bareese@slac.stanford.edu>
2024-11-11 11:41:35 -0800

    Use WarmTdmRoot to allow standalone row board operation

--- firmware/python/warm_tdm/_WarmTdmRoot.py
+++ firmware/python/warm_tdm/_WarmTdmRoot.py
@@ -39,15 +39,20 @@ class StreamDebug(rogue.interfaces.stream.Slave):
 class WarmTdmRoot(pyrogue.Root):
     def __init__(
             self,
-            host,
-            rowBoards,
-            colBoards,
-            frontEndClass,            
+            colBoardClass,
+            colFeClass,
+            rowBoardClass,
+            rowFeClass,
             simulation=False,
             emulate=False,
-            plots=False,
-
+            host='192.168.3.11',
+            colBoards=4,
+            rowBoards=1,
+            numRows=32,
             **kwargs):
+
+        if 'groupConfig' in kwargs:
+            del kwargs['groupConfig']
 
         # Disable polling and set a longer timeout in simulation mode
         if simulation:
@@ -59,7 +64,7 @@ class WarmTdmRoot(pyrogue.Root):
 
         self.zmqServer = pyrogue.interfaces.ZmqServer(root=self, addr='127.0.0.1', port=0)
         self.addInterface(self.zmqServer)
-        self.add(pyrogue.utilities.fileio.StreamWriter(name='DataWriter',groups='DocApi'))        
+        self.add(pyrogue.utilities.fileio.StreamWriter(name='DataWriter',groups='DocApi'))
 
         # Add the data writer
         #self.add(pyrogue.utilities.fileio.StreamWriter(name='DataWriter'))
@@ -67,72 +72,16 @@ class WarmTdmRoot(pyrogue.Root):
 
         self.add(warm_tdm.HardwareGroup(
             groupId=0,
-            rows=32,
-            frontEndClass=frontEndClass,
             dataWriter=self.DataWriter,
             simulation=simulation,
             emulate=emulate,
             host=host,
             colBoards=colBoards,
+            colBoardClass=colBoardClass,
+            colFeClass=colFeClass,
             rowBoards=rowBoards,
-            plots=plots,
-#            groups=['Hardware'],
+            rowBoardClass=rowBoardClass,
+            rowFeClass=rowFeClass,
+            rows=numRows,
+            groups=['Hardware'],
             expand=True))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # Add the data writer
-#            dataStream >> self.DataWriter.getChannel(index)
-
-            # Connect to simulated PGP ring
-#             if simPorts is not None:
-#                 pgp = pyrogue.interfaces.simulation.Pgp2bSim(vcCount=4, host='localhost', port=SIM_PORTS[index])
-#                 pgpRing.append(pgp)
-#                 self.addInterface(pgp) # Should modify Pgp2bSim to stop the TcpClients on _stop
-
-#         numBoards = len(pgpRing)
-#         for i in range(numBoards):
-#             print(f'Linking board {i} to board {(i+1)%numBoards}')
-#             pgpRing[i].vc[0] >> pgpRing[(i+1)%numBoards].vc[0]
-#             pgpRing[i].sb.setRecvCb(pgpRing[(i+1)%numBoards].sb.send)
-
-#         pgpRing[0].vc[0] >> pgpRing[1].vc[0] >> pgpRing[2].vc[0] >> pgpRing[3].vc[0] >> pgpRing[4].vc[0] >> pgpRing[5].vc[0] >> pgpRing[0].vc[0]
-#         pgpRing[0].sb.setRecvCb(pgpRing[1].sb.send)
-#         pgpRing[1].sb.setRecvCb(pgpRing[2].sb.send)
-#         pgpRing[2].sb.setRecvCb(pgpRing[3].sb.send)
-#         pgpRing[3].sb.setRecvCb(pgpRing[4].sb.send)
-#         pgpRing[4].sb.setRecvCb(pgpRing[5].sb.send)
-#         pgpRing[5].sb.setRecvCb(pgpRing[0].sb.send)
-        #pgpRing[0].sb.send(0, 0)
-
-#         rowModules = [x for x in self.deviceList if isinstance(x, warm_tdm.RowModule)]
-#         if len(rowModules) > 0:
-#             self.add(warm_tdm.RowSelectArray(rowModules, enabled=False))
-
-#             if index != 0:
-#                 for ch in range(4):
-#                     pgpRing[0].vc[ch] >> pgpRing[1].vc[ch]
-#                     pgpRing[index-1].vc[ch] >> pgpRing[index].vc[ch]
-
-#                 print(f'Linking index {index-1} to index {index}')
-#                 pgpRing[index-1].sb.setRecvCb(pgpRing[index].sb.send)
-
-
-#             if index == len(stack)-1:
-#                 for vc in range(4):
-#                     pgpRing[index].vc[ch] >> pgpRing[0].vc[ch]
-
-#                 print(f'Linking index {index} to index 0')
-#                 pgpRing[index].sb.setRecvCb(pgpRi
