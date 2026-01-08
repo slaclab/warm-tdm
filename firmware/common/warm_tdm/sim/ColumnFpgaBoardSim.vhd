bc8de1df739bdd14e5cddf2ff25c3d6a64b4d7d0
Benjamin Reese <bareese@slac.stanford.edu>
2026-01-08 14:35:58 -0800

    Add new simulation for column and row board

---
+++ firmware/common/warm_tdm/sim/ColumnFpgaBoardSim.vhd
@@ -0,0 +1,207 @@
+-------------------------------------------------------------------------------
+-- Title      : 
+-------------------------------------------------------------------------------
+-- Company    : SLAC National Accelerator Laboratory
+-- Platform   : 
+-- Standard   : VHDL'93/02
+-------------------------------------------------------------------------------
+-- Description: Simulation of Column FPGA board, Column FEB and Load board
+-------------------------------------------------------------------------------
+-- This file is part of Warm TDM. It is subject to
+-- the license terms in the LICENSE.txt file found in the top-level directory
+-- of this distribution and at:
+--    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+-- No part of Warm TDM, including this file, may be
+-- copied, modified, propagated, or distributed except according to the terms
+-- contained in the LICENSE.txt file.
+-------------------------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+
+library surf;
+use surf.StdRtlPkg.all;
+
+library warm_tdm;
+use warm_tdm.SimPkg.all;
+
+entity ColumnFpgaBoardSim is
+   generic (
+      TPD_G                   : time                  := 1 ns;
+      RING_ADDR_0_G           : boolean               := false;
+      SIM_PGP_PORT_NUM_G      : integer               := 7000;
+      SIM_ETH_SRP_PORT_NUM_G  : integer               := 8000;
+      SIM_ETH_DATA_PORT_NUM_G : integer               := 9000;
+      AWAXE_G                 : boolean               := false;
+      ADC_R_GAIN_G            : RealArray(7 downto 0) := (others => 1.00e3);
+      ADC_R_FB_G              : RealArray(7 downto 0) := (others => 3.66e3));
+
+   port (
+      -- component ports
+      rj45TimingRxClkP  : in  sl;
+      rj45TimingRxClkN  : in  sl;
+      rj45TimingRxDataP : in  sl;
+      rj45TimingRxDataN : in  sl;
+      rj45TimingTxClkP  : out sl;
+      rj45TimingTxClkN  : out sl;
+      rj45TimingTxDataP : out sl;
+      rj45TimingTxDataN : out sl);
+
+end entity ColumnFpgaBoardSim;
+
+architecture sim of ColumnFpgaBoardSim is
+
+   signal feThermistor : slv(1 downto 0) := "00";
+   signal feI2cScl     : slv(3 downto 0);
+   signal feI2cSda     : slv(3 downto 0);
+   signal resetB       : sl;
+   signal feVrSyncA    : sl;
+   signal feVrSyncB    : sl;
+   signal feDacMosi    : sl;
+   signal feDacMiso    : sl;
+   signal feDacSclk    : sl;
+   signal feDacSyncB   : slv(2 downto 0);
+   signal feDacLdacB   : slv(2 downto 0) := "111";
+   signal feDacResetB  : slv(2 downto 0) := "111";
+   signal tesDelatch   : slv(7 downto 0) := (others => '0');
+   signal saFbDacP     : RealArray(7 downto 0);
+   signal saFbDacN     : RealArray(7 downto 0);
+   signal sq1FbDacP    : RealArray(7 downto 0);
+   signal sq1FbDacN    : RealArray(7 downto 0);
+   signal sq1BiasDacP  : RealArray(7 downto 0);
+   signal sq1BiasDacN  : RealArray(7 downto 0);
+   signal auxDacP      : RealArray(7 downto 0);
+   signal auxDacN      : RealArray(7 downto 0);
+   signal saOutP       : RealArray(7 downto 0);
+   signal saOutN       : RealArray(7 downto 0);
+
+   type CurrentArray8Array is array (natural range <>) of CurrentArray(7 downto 0);
+
+   signal tesBiasP   : RealArray(7 downto 0);
+   signal tesBiasN   : RealArray(7 downto 0);
+   signal saBiasOutP : CurrentArray(7 downto 0);
+   signal saBiasOutN : CurrentArray(7 downto 0);
+   signal saBiasInP  : RealArray(7 downto 0);
+   signal saBiasInN  : RealArray(7 downto 0);
+   signal saFbP      : CurrentArray(7 downto 0);
+   signal saFbN      : CurrentArray(7 downto 0);
+   signal sq1BiasP   : CurrentArray(7 downto 0);
+   signal sq1BiasN   : CurrentArray(7 downto 0);
+   signal sq1FbP     : CurrentArray(7 downto 0);
+   signal sq1FbN     : CurrentArray(7 downto 0);
+
+begin
+
+   U_ColumnFpgaBoard : entity warm_tdm.ColumnFpgaBoardModel
+      generic map (
+         TPD_G                   => TPD_G,
+         RING_ADDR_0_G           => RING_ADDR_0_G,
+         SIM_PGP_PORT_NUM_G      => SIM_PGP_PORT_NUM_G,
+         SIM_ETH_SRP_PORT_NUM_G  => SIM_ETH_SRP_PORT_NUM_G,
+         SIM_ETH_DATA_PORT_NUM_G => SIM_ETH_DATA_PORT_NUM_G)
+      port map (
+         feThermistor      => feThermistor,
+         feI2cScl          => feI2cScl,
+         feI2cSda          => feI2cSda,
+         resetB            => resetB,
+         feVrSyncA         => feVrSyncA,
+         feVrSyncB         => feVrSyncB,
+         feDacMosi         => feDacMosi,
+         feDacMiso         => feDacMiso,
+         feDacSclk         => feDacSclk,
+         feDacSyncB        => feDacSyncB,
+         feDacLdacB        => feDacLdacB,
+         feDacResetB       => feDacResetB,
+         tesDelatch        => tesDelatch,
+         saFbDacP          => saFbDacP,
+         saFbDacN          => saFbDacN,
+         sq1FbDacP         => sq1FbDacP,
+         sq1FbDacN         => sq1FbDacN,
+         sq1BiasDacP       => sq1BiasDacP,
+         sq1BiasDacN       => sq1BiasDacN,
+         auxDacP           => auxDacP,
+         auxDacN           => auxDacN,
+         saOutP            => saOutP,
+         saOutN            => saOutN,
+         rj45TimingRxClkP  => rj45TimingRxClkP,   -- [in]
+         rj45TimingRxClkN  => rj45TimingRxClkN,   -- [in]
+         rj45TimingRxDataP => rj45TimingRxDataP,  -- [in]
+         rj45TimingRxDataN => rj45TimingRxDataN,  -- [in] 
+         rj45TimingRxMgtP  => '0',  -- TODO: connect this to something?                   -- [in]
+         rj45TimingRxMgtN  => '1',  -- TODO: connect this to something?                   -- [in]
+         rj45PgpRxMgtP     => '0',  -- TODO: connect this to something?                   -- [in]
+         rj45PgpRxMgtN     => '1',  -- TODO: connect this to something?                   -- [in]
+         rj45TimingTxClkP  => rj45TimingTxClkP,   -- [out]
+         rj45TimingTxClkN  => rj45TimingTxClkN,   -- [out]
+         rj45TimingTxDataP => rj45TimingTxDataP,  -- [out]
+         rj45TimingTxDataN => rj45TimingTxDataN,  -- [out]
+         rj45TimingTxMgtP  => open,     -- [out]
+         rj45TimingTxMgtN  => open,     -- [out]
+         rj45PgpTxMgtP     => open,     -- [out]
+         rj45PgpTxMgtN     => open);    -- [out]
+
+   U_ColumnReadoutFebModel_1 : entity warm_tdm.ColumnReadoutFebModel
+      generic map (
+         TPD_G   => TPD_G,
+         AWAXE_G => AWAXE_G)
+      port map (
+         -- FEB Connector
+         feThermistor => feThermistor,  -- [out]
+         feI2cScl     => feI2cScl,      -- [inout]
+         feI2cSda     => feI2cSda,      -- [inout]
+         resetB       => resetB,        -- [in]
+         feVrSyncA    => feVrSyncA,     -- [in]
+         feVrSyncB    => feVrSyncB,     -- [in]
+         feDacMosi    => feDacMosi,     -- [in]
+         feDacMiso    => feDacMiso,     -- [out]
+         feDacSclk    => feDacSclk,     -- [in]
+         feDacSyncB   => feDacSyncB,    -- [in]
+         feDacLdacB   => feDacLdacB,    -- [in]
+         feDacResetB  => feDacResetB,   -- [in]
+         tesDelatch   => tesDelatch,    -- [in]
+         saFbDacP     => saFbDacP,      -- [in]
+         saFbDacN     => saFbDacN,      -- [in]
+         sq1FbDacP    => sq1FbDacP,     -- [in]
+         sq1FbDacN    => sq1FbDacN,     -- [in]
+         sq1BiasDacP  => sq1BiasDacP,   -- [in]
+         sq1BiasDacN  => sq1BiasDacN,   -- [in]
+         auxDacP      => auxDacP,       -- [in]
+         auxDacN      => auxDacN,       -- [in]
+         saSigOutP    => saOutP,        -- [out]
+         saSigOutN    => saOutN,        -- [out]
+         -- CRYO connector
+         tesBiasP     => tesBiasP,      -- [out]
+         tesBiasN     => tesBiasN,      -- [out]
+         saBiasOutP   => saBiasOutP,    -- [out]
+         saBiasOutN   => saBiasOutN,    -- [out]
+         saBiasInP    => saBiasInP,     -- [in]
+         saBiasInN    => saBiasInN,     -- [in]
+         saFbP        => saFbP,         -- [out]
+         saFbN        => saFbN,         -- [out]
+         sq1BiasP     => sq1BiasP,      -- [out]
+         sq1BiasN     => sq1BiasN,      -- [out]
+         sq1FbP       => sq1FbP,        -- [out]
+         sq1FbN       => sq1FbN);       -- [out]
+
+   U_ColumnLoadBoard_1 : entity warm_tdm.ColumnLoadBoard
+--         generic map (
+--             SA_BIAS_LOADS_G  => SA_BIAS_LOADS_G,
+--             SA_FB_LOADS_G    => SA_FB_LOADS_G,
+--             SQ1_BIAS_LOADS_G => SQ1_BIAS_LOADS_G,
+--             SQ1_FB_LOADS_G   => SQ1_FB_LOADS_G)
+      port map (
+         tesBiasP   => tesBiasP,        -- [in]
+         tesBiasN   => tesBiasN,        -- [in]
+         saBiasOutP => saBiasOutP,      -- [in]
+         saBiasOutN => saBiasOutN,      -- [in]
+         saBiasInP  => saBiasInP,       -- [out]
+         saBiasInN  => saBiasInN,       -- [out]
+         saFbP      => saFbP,           -- [in]
+         saFbN      => saFbN,           -- [in]
+         sq1BiasP   => sq1BiasP,        -- [in]
+         sq1BiasN   => sq1BiasN,        -- [in]
+         sq1FbP     => sq1FbP,          -- [in]
+         sq1FbN     => sq1FbN);         -- [in]
+
+
+
+end architecture sim;
---
+++ firmware/simulations/GroupTb/tb/GroupTb.vhd
@@ -0,0 +1,123 @@
+-------------------------------------------------------------------------------
+-- File       : RceSimTb.vhd
+-- Company    : SLAC National Accelerator Laboratory
+-- Created    : 2018-06-23
+-- Last update: 2024-06-11
+-------------------------------------------------------------------------------
+-- Description: Simulation Testbed for testing the SimpleRogueSim module
+-------------------------------------------------------------------------------
+-- This file is part of 'ATLAS RD53 DEV'.
+-- It is subject to the license terms in the LICENSE.txt file found in the 
+-- top-level directory of this distribution and at: 
+--    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+-- No part of 'ATLAS RD53 DEV', including this file, 
+-- may be copied, modified, propagated, or distributed except according to 
+-- the terms contained in the LICENSE.txt file.
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.std_logic_arith.all;
+
+
+library surf;
+use surf.StdRtlPkg.all;
+
+library warm_tdm;
+
+entity GroupTb is
+end GroupTb;
+
+architecture sim of GroupTb is
+
+   constant SIM_PGP_GT_C : boolean := true;
+
+   constant COLUMN_BOARDS_C : integer := 1;
+   constant ROW_BOARDS_C    : integer := 1;
+
+   constant AWAXE_G : boolean := false;
+
+   constant NUM_ROW_SELECTS_G  : integer range 1 to 32 := 10;
+   constant NUM_CHIP_SELECTS_G : integer range 0 to 8  := 6;
+
+   constant GROUP_SIZE_C : integer := COLUMN_BOARDS_C + ROW_BOARDS_C;
+   constant TPD_G        : time    := 1 ns;
+
+   -------------------------------------------------------------------------------------------------
+   -- Ring network
+   -- Pgp carried on rj45TimingMgt for compatibility with older Column Module
+   -------------------------------------------------------------------------------------------------
+   signal rj45TimingClkP  : slv(GROUP_SIZE_C-1 downto 0);
+   signal rj45TimingClkN  : slv(GROUP_SIZE_C-1 downto 0);
+   signal rj45TimingDataP : slv(GROUP_SIZE_C-1 downto 0);
+   signal rj45TimingDataN : slv(GROUP_SIZE_C-1 downto 0);
+   signal rj45TimingMgtP  : slv(GROUP_SIZE_C-1 downto 0);
+   signal rj45TimingMgtN  : slv(GROUP_SIZE_C-1 downto 0);
+   signal rj45PgpMgtP     : slv(GROUP_SIZE_C-1 downto 0);
+   signal rj45PgpMgtN     : slv(GROUP_SIZE_C-1 downto 0);
+
+begin
+
+   GEN_COL_BOARDS : for i in 0 to COLUMN_BOARDS_C-1 generate
+      U_ColumnModuleBoard : entity warm_tdm.ColumnFpgaBoardSim
+         generic map (
+            TPD_G                   => TPD_G,
+            RING_ADDR_0_G           => (i = 0),
+            AWAXE_G                 => AWAXE_G,
+            SIM_PGP_PORT_NUM_G      => 7000 + (40 *i),  --ite(SIM_PGP_GT_C, 0, 7000),
+            SIM_ETH_SRP_PORT_NUM_G  => 10000 + (i * 1000),
+            SIM_ETH_DATA_PORT_NUM_G => 20000 + (i * 1000))
+         port map (
+            -- Incomming connections from last in loop
+            rj45TimingRxClkP  => rj45TimingClkP(ite(i = 0, GROUP_SIZE_C-1, i-1)),   -- [in]
+            rj45TimingRxClkN  => rj45TimingClkN(ite(i = 0, GROUP_SIZE_C-1, i-1)),   -- [in]
+            rj45TimingRxDataP => rj45TimingDataP(ite(i = 0, GROUP_SIZE_C-1, i-1)),  -- [in]
+            rj45TimingRxDataN => rj45TimingDataN(ite(i = 0, GROUP_SIZE_C-1, i-1)),  -- [in]
+--            rj45TimingRxMgtP  => rj45TimingMgtP((i+GROUP_SIZE_C-1) mod GROUP_SIZE_C),   -- [in]
+--            rj45TimingRxMgtN  => rj45TimingMgtN((i+GROUP_SIZE_C-1) mod GROUP_SIZE_C),   -- [in]
+--            rj45PgpRxMgtP     => rj45PgpMgtP((i+GROUP_SIZE_C-1) mod GROUP_SIZE_C),      -- [in]
+--            rj45PgpRxMgtN     => rj45PgpMgtN((i+GROUP_SIZE_C-1) mod GROUP_SIZE_C),      -- [in]
+            -- Outgoing connections
+            rj45TimingTxClkP  => rj45TimingClkP(i),     -- [out]
+            rj45TimingTxClkN  => rj45TimingClkN(i),     -- [out]
+            rj45TimingTxDataP => rj45TimingDataP(i),    -- [out]
+            rj45TimingTxDataN => rj45TimingDataN(i));   -- [out]
+--             rj45TimingTxMgtP  => rj45TimingMgtP(i),     -- [out]
+--             rj45TimingTxMgtN  => rj45TimingMgtN(i),     -- [out]
+--             rj45PgpTxMgtP     => rj45PgpMgtP(i),        -- [out]
+--             rj45PgpTxMgtN     => rj45PgpMgtN(i));       -- [out]
+
+   end generate GEN_COL_BOARDS;
+
+   GEN_ROW_BOARDS : for i in COLUMN_BOARDS_C to GROUP_SIZE_C-1 generate
+      U_RowModuleBoard : entity warm_tdm.RowFpgaBoardSim
+         generic map (
+            TPD_G                   => TPD_G,
+            RING_ADDR_0_G           => (i = 0),
+            SIM_PGP_PORT_NUM_G      => 70000 + (40*i),                              --7000 + 40,
+            SIM_ETH_SRP_PORT_NUM_G  => 10000 + (i * 1000),
+            SIM_ETH_DATA_PORT_NUM_G => 20000 + (i * 1000),
+            NUM_ROW_SELECTS_G       => NUM_ROW_SELECTS_G,
+            NUM_CHIP_SELECTS_G      => NUM_CHIP_SELECTS_G)
+         port map (
+            rj45TimingRxClkP  => rj45TimingClkP(ite(i = 0, GROUP_SIZE_C-1, i-1)),   -- [in]
+            rj45TimingRxClkN  => rj45TimingClkN(ite(i = 0, GROUP_SIZE_C-1, i-1)),   -- [in]
+            rj45TimingRxDataP => rj45TimingDataP(ite(i = 0, GROUP_SIZE_C-1, i-1)),  -- [in]
+            rj45TimingRxDataN => rj45TimingDataN(ite(i = 0, GROUP_SIZE_C-1, i-1)),  -- [in]
+--             rj45TimingRxMgtP  => rj45TimingMgtP((i+GROUP_SIZE_C-1) mod GROUP_SIZE_C),   -- [in]
+--             rj45TimingRxMgtN  => rj45TimingMgtN((i+GROUP_SIZE_C-1) mod GROUP_SIZE_C),   -- [in]
+--             rj45PgpRxMgtP     => rj45PgpMgtP((i+GROUP_SIZE_C-1) mod GROUP_SIZE_C),      -- [in]
+--             rj45PgpRxMgtN     => rj45PgpMgtN((i+GROUP_SIZE_C-1) mod GROUP_SIZE_C),      -- [in]
+            rj45TimingTxClkP  => rj45TimingClkP(i),                                 -- [out]
+            rj45TimingTxClkN  => rj45TimingClkN(i),                                 -- [out]
+            rj45TimingTxDataP => rj45TimingDataP(i),                                -- [out]
+            rj45TimingTxDataN => rj45TimingDataN(i));                               -- [out]
+--             rj45TimingTxMgtP  => rj45TimingMgtP(i),                                     -- [out]
+--             rj45TimingTxMgtN  => rj45TimingMgtN(i),                                     -- [out]
+--             rj45PgpTxMgtP     => rj45PgpMgtP(i),                                        -- [out]
+--             rj45PgpTxMgtN     => rj45PgpMgtN(i));                                       -- [out]
+
+   end generate GEN_ROW_BOARDS;
+
+end sim;
