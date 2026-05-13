b9aa42dc76562f0e0c00e87f89d14d408ffa84ce
Benjamin Reese <bareese@slac.stanford.edu>
2026-05-13 13:52:04 -0700

    Update comment headers.

--- firmware/targets/ColumnFpgaBoardAwaXe/ruckus.tcl
+++ firmware/targets/ColumnFpgaBoardAwaXe/ruckus.tcl
@@ -3,7 +3,7 @@
 ## It is subject to the license terms in the LICENSE.txt file found in the 
 ## top-level directory of this distribution and at: 
 ##    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
-## No part of 'kpix-dev', including this file, 
+## No part of 'warm-tdm', including this file, 
 ## may be copied, modified, propagated, or distributed except according to 
 ## the terms contained in the LICENSE.txt file.
 ##############################################################################
