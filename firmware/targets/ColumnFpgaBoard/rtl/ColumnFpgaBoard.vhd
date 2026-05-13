d52960350166c737120718243600b6c60170012e
Benjamin Reese <bareese@slac.stanford.edu>
2026-05-13 13:51:19 -0700

    Remove ADC filter by default.

--- firmware/targets/ColumnFpgaBoard/rtl/ColumnFpgaBoard.vhd
+++ firmware/targets/ColumnFpgaBoard/rtl/ColumnFpgaBoard.vhd
@@ -53,7 +53,7 @@ entity ColumnFpgaBoard is
       DHCP_G                  : boolean              := false;
       IP_ADDR_G               : slv(31 downto 0)     := x"0B03A8C0";  -- 192.168.3.11
       MAC_ADDR_G              : slv(47 downto 0)     := x"0B_00_16_56_00_08";
-      GEN_ADC_FILTER_G        : boolean              := true;
+      GEN_ADC_FILTER_G        : boolean              := false;
       ROW_ADDR_BITS_G         : integer range 3 to 8 := 8);
    port (
       -- Clocks
