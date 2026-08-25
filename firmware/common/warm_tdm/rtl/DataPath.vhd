5497f54e490a6df2fce87800dab6beb79eeaa2f7
Benjamin Reese <bareese@slac.stanford.edu>
2026-08-25 12:07:50 -0700

    Set default delays for ADC.

--- firmware/common/warm_tdm/rtl/DataPath.vhd
+++ firmware/common/warm_tdm/rtl/DataPath.vhd
@@ -216,12 +216,16 @@ begin
    -------------------------------------------------------------------------------------------------
    U_Ad9681Readout_1 : entity surf.Ad9681Readout
       generic map (
-         TPD_G            => TPD_G,
-         AXIL_BASE_ADDR_G => XBAR_COFNIG_C(ADC_READOUT_AXIL_C).baseAddr,
-         IODELAY_GROUP_G  => IODELAY_GROUP_G,
-         DEVICE_FAMILY_G  => "7SERIES",
-         LEFT_JUSTIFY_G   => true,
-         NEGATE_G         => NEGATE_ADC_G)
+         TPD_G             => TPD_G,
+         AXIL_BASE_ADDR_G  => XBAR_COFNIG_C(ADC_READOUT_AXIL_C).baseAddr,
+         IODELAY_GROUP_G   => IODELAY_GROUP_G,
+         DEVICE_FAMILY_G   => "7SERIES",
+         -- Bench calibration consistently centers every FCO and data lane near
+         -- tap 11, so seed all delays there for a near-aligned power-up.
+         DATA_DELAY_INIT_G => (others => 11),
+         FCO_DELAY_INIT_G  => (0 => 16, 1 => 10),
+         LEFT_JUSTIFY_G    => true,
+         NEGATE_G          => NEGATE_ADC_G)
       port map (
          axilClk         => timingRxClk125,                           -- [in]
          axilRst         => timingRxRst125,                           -- [in]
