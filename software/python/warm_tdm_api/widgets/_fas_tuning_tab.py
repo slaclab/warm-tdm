df7e710d61577694733197c39b37b8be5c4d7da1
Benjamin Reese <bareese@slac.stanford.edu>
2026-09-18 15:28:21 -0700

    Fix fas tune widget layout.

--- software/python/warm_tdm_api/widgets/_fas_tuning_tab.py
+++ software/python/warm_tdm_api/widgets/_fas_tuning_tab.py
@@ -7,9 +7,13 @@
 from pydm.widgets import PyDMSpinbox
 
 from warm_tdm_api.widgets._tuning_tab import TuningTab
+from warm_tdm_api.widgets._two_column_process import TwoColumnProcess
 
 
 class FasTuningTab(TuningTab):
+
+    def _process_widget(self):
+        return TwoColumnProcess(init_channel=self._process_channel())
 
     def _plot_channels(self):
         return [
